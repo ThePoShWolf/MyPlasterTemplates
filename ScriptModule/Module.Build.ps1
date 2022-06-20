ca0cef87772093284cac0de56eee15e2ee4f21da
Anthony Howell <anthony@howell-it.com>
2022-06-20 14:34:21 -0700

    updating, removing psdeploy

--- ScriptModule/Module.Build.ps1
+++ ScriptModule/Module.Build.ps1
@@ -1,12 +1,16 @@
+param (
+    [version]$Version = '0.0.1',
+    [string]$NugetApiKey,
+    [string]$PreRelease
+)
 $srcPath = "$PSScriptRoot\src"
 $buildPath = "$PSScriptRoot\build"
 $docPath = "$PSScriptRoot\docs"
 $testPath = "$PSScriptRoot\tests"
 $moduleName = ($MyInvocation.MyCommand.Name.Split('.') | Select-Object -SkipLast 2) -join '.'
 $modulePath = "$buildPath\$ModuleName"
-$version = '0.0.1'
 
-
+Write-Host "Version: $($version)"
 
 # Clean out any previous builds
 task Clean {
@@ -34,45 +38,64 @@ task ModuleBuild Clean, {
     }
 
     # Add using.ps1 to the .psm1 first
-    foreach ($file in $moduleScriptFiles | ?{$_.Name -eq 'using.ps1'}) {
+    foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -eq 'using.ps1' }) {
         if ($file.fullname) {
+            Write-Host "Adding using file: '$($file.Fullname)'"
             Get-Content $file.fullname | Out-File "$modulePath\$moduleName.psm1" -Append -Encoding utf8
         }
     }
 
-    # Add all .ps1 files to the .psm1
-    foreach ($file in $moduleScriptFiles | ?{$_.Name -ne 'onload.ps1' -and $_.Name -ne 'using.ps1'}) {
+    # Add all .ps1 files to the .psm1, skipping onload.ps1, using.ps1, and any tests
+    foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -ne 'onload.ps1' -and $_.Name -ne 'using.ps1' -and $_.FullName -notmatch '(\\|\/)tests(\\|\/)[^\.]+\.tests\.ps1$' }) {
         if ($file.fullname) {
+            Write-Host "Adding function file: '$($file.FullName)'"
             Get-Content $file.fullname | Out-File "$modulePath\$moduleName.psm1" -Append -Encoding utf8
         }
     }
     
     # Add the onload.ps1 files last
-    foreach ($file in $moduleScriptFiles | ?{$_.Name -eq 'onload.ps1'}) {
+    foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -eq 'onload.ps1' }) {
         if ($file.fullname) {
+            Write-Host "Adding onload file: '$($file.FullName)'"
             Get-Content $file.fullname | Out-File "$modulePath\$moduleName.psm1" -Append -Encoding utf8
         }
     }
 
+    # Copy any .dlls
+    Copy-Item $PSScriptRoot\lib -Destination $modulePath -Recurse -Force -ErrorAction SilentlyContinue
+
     # Copy the manifest
     Copy-Item "$srcPath\$moduleName.psd1" -Destination $modulePath
 
+    # Copy the tests
+    foreach ($test in ($moduleScriptFiles | Where-Object { $_.FullName -match '(\\|\/)tests(\\|\/)[^\.]+\.tests\.ps1$' })) {
+        Write-Host "Copying test file: '$($test.FullName)'"
+        Copy-Item $test.FullName -Destination $modulePath
+    }
+
     $moduleManifestData = @{
-        Path = "$modulePath\$moduleName.psd1"
+        Path              = "$modulePath\$moduleName.psd1"
         # Only export the public files
-        FunctionsToExport = ($moduleScriptFiles | Where-Object {$_.FullName -match "\\public\\[^\.]+\.ps1$"}).basename
-        ModuleVersion = $version
+        FunctionsToExport = ($moduleScriptFiles | Where-Object { $_.FullName -match "(\\|\/)public(\\|\/)[^\.]+\.ps1$" }).basename
+        ModuleVersion     = $version
+    }
+    if ($null -ne $PreRelease) {
+        $moduleManifestData['Prerelease'] = $PreRelease
     }
     Update-ModuleManifest @moduleManifestData
 }
 
-Task Test ModuleBuild, {
+task Test ModuleBuild, {
+    Write-Host "Importing module."
     Import-Module $modulePath -RequiredVersion $version
-    Invoke-Pester $testPath
+    Write-Host "Invoking tests."
+    Invoke-Pester $testPath -Verbose
 }
 
 task Publish Test, DocBuild, {
-    Invoke-PSDeploy -Force
+    if ($null -ne $NugetApiKey) {
+        Publish-Module -Path .\build\$moduleName -NuGetApiKey $NugetApiKey -Repository PsGallery
+    }
 }
 
 task All ModuleBuild, Publish
--- ScriptModule/Module.PSDeploy.ps1
+++
@@ -1,9 +0,0 @@
-Deploy "$($MyInvocation.MyCommand.Name.Split('.')[0])" {
-    By PSGalleryModule {
-        FromSource "Build\$($MyInvocation.MyCommand.Name.Split('.')[0])"
-        To PSGallery
-        WithOptions @{
-            ApiKey = ''
-        }
-    }
-}
